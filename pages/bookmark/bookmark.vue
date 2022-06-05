dacc3bbada2fa113f417b37d7891e46b189dbb4f
xugw <2279799268@qq.com>
2022-06-05 20:57:58 +0800

    完成书签页+作品页

--- pages/bookmark/bookmark.vue
+++ pages/bookmark/bookmark.vue
@@ -1,22 +1,75 @@
 <template>
 	<view>
-		
+		<u-index-list :index-list="indexList.index">
+			<template v-for="(item, index) in itemArr">
+				<u-index-item>
+					<u-index-anchor :text="indexList.text[index]"></u-index-anchor>
+					<u-cell v-for="(cell, cindex) in item" :key="cindex">
+						<u-avatar slot="icon" shape="square" size="35" :src="avatarList[index][cindex]" customStyle="margin: -3px 5px -3px 0"></u-avatar>
+						<u-link slot="title" :href="cell.url" :text="cell.title" color="#0b87da"></u-link>
+					</u-cell>
+				</u-index-item>
+			</template>
+		</u-index-list>
+		<tabbar :tabValue="2"></tabbar>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
+import tabbar from '../../components/tabbar.vue';
+
+export default {
+	components: { tabbar },
+	data() {
+		return {
+			indexList: { index: ['A', 'B', 'C', 'D'], text: ['A - 前端库', 'B - vue 相关', 'C - 其他', 'D - 文档'] },
+			itemArr: [
+				[
+					{ title: 'bootstrap', url: 'https://github.com/twbs/bootstrap' },
+					{ title: 'echarts', url: 'https://github.com/apache/echarts' },
+					{ title: 'uni-app', url: 'https://github.com/dcloudio/uni-app' },
+					{ title: 'uView', url: 'https://github.com/umicro/uView2.0' }
+				],
+				[
+					{ title: 'vue', url: 'https://github.com/vuejs/vue' },
+					{ title: 'vue-cli', url: 'https://github.com/vuejs/vue-cli' },
+					{ title: 'vuex', url: 'https://github.com/vuejs/vuex' },
+					{ title: 'vue router', url: 'https://github.com/vuejs/router' },
+					{ title: 'element-ui', url: 'https://github.com/ElemeFE/element' }
+				],
+				[
+					{ title: 'axios', url: 'https://github.com/axios/axios' },
+					{ title: 'mockjs', url: 'https://github.com/nuysoft/Mock' },
+					{ title: 'express', url: 'https://github.com/expressjs/express' },
+					{ title: 'django', url: 'https://github.com/django/django' }
+				],
+				[
+					{ title: 'docsify', url: 'https://github.com/docsifyjs/docsify' },
+					{ title: 'vuepress', url: 'https://github.com/vuejs/vuepress' },
+					{ title: 'hexo', url: 'https://github.com/hexojs/hexo' }
+				]
+			],
+			avatarList: [[], [], [], []],
+			urls: [
+				'https://cdn.uviewui.com/uview/album/1.jpg',
+				'https://cdn.uviewui.com/uview/album/2.jpg',
+				'https://cdn.uviewui.com/uview/album/3.jpg',
+				'https://cdn.uviewui.com/uview/album/4.jpg',
+				'https://cdn.uviewui.com/uview/album/5.jpg',
+				'https://cdn.uviewui.com/uview/album/6.jpg',
+				'https://cdn.uviewui.com/uview/album/7.jpg',
+				'https://cdn.uviewui.com/uview/album/8.jpg',
+				'https://cdn.uviewui.com/uview/album/9.jpg',
+				'https://cdn.uviewui.com/uview/album/10.jpg'
+			]
+		};
+	},
+	onLoad() {
+		for (let i = 0; i < 4; i++) {
+			for (let j = 0; j < this.itemArr[i].length; j++) {
+				this.avatarList[i].push(this.urls[uni.$u.random(0, this.urls.length - 1)]);
 			}
-		},
-		methods: {
-			
 		}
 	}
+};
 </script>
-
-<style>
-
-</style>
--- pages/works/works.vue
+++ pages/works/works.vue
@@ -1,22 +1,59 @@
 <template>
-	<view>
-		
+	<view class="content">
+		<u-list>
+			<u-list-item v-for="(item, index) in indexList" :key="index">
+				<u-cell>
+					<u-avatar slot="icon" shape="square" size="35" :src="item.url" customStyle="margin: -3px 5px -3px 0"></u-avatar>
+					<u-link slot="title" :href="item.linkUrl" :text="item.title" color="#19be6b"></u-link>
+				</u-cell>
+			</u-list-item>
+		</u-list>
+		<tabbar :tabValue="3"></tabbar>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
+import tabbar from '../../components/tabbar.vue';
+
+export default {
+	components: { tabbar },
+	data() {
+		return {
+			indexList: [],
+			urls: [
+				'https://cdn.uviewui.com/uview/album/1.jpg',
+				'https://cdn.uviewui.com/uview/album/2.jpg',
+				'https://cdn.uviewui.com/uview/album/3.jpg',
+				'https://cdn.uviewui.com/uview/album/4.jpg',
+				'https://cdn.uviewui.com/uview/album/5.jpg',
+				'https://cdn.uviewui.com/uview/album/6.jpg',
+				'https://cdn.uviewui.com/uview/album/7.jpg',
+				'https://cdn.uviewui.com/uview/album/8.jpg',
+				'https://cdn.uviewui.com/uview/album/9.jpg',
+				'https://cdn.uviewui.com/uview/album/10.jpg'
+			],
+			titleList: ['vue-admin', '博客案例', '记事本', '实时天气', '番茄时钟', '计算器', '鼓式机器', 'Markdown编辑器', '随机引语'],
+			linkUrls: [
+				'https://vue-admin-mu.vercel.app/#/',
+				'https://blog-case.vercel.app/#/',
+				'https://codepen.io/xugw/full/poppMxP',
+				'https://codepen.io/xugw/full/NWXXZoV',
+				'https://codepen.io/xugw/full/VwyKyJo',
+				'https://codepen.io/xugw/full/VwyKpEO',
+				'https://codepen.io/xugw/full/JjMKMzV',
+				'https://codepen.io/xugw/full/BaJKXNo',
+				'https://codepen.io/xugw/full/YzYLMOe'
+			]
+		};
+	},
+	onLoad() {
+		for (let i = 0; i < 9; i++) {
+			this.indexList.push({
+				title: this.titleList[i],
+				url: this.urls[uni.$u.random(0, this.urls.length - 1)],
+				linkUrl: this.linkUrls[i]
+			});
 		}
 	}
+};
 </script>
-
-<style>
-
-</style>
